5534b30d69e51b666cf0fd13d78930c7db51304e
Marwen Boussabat <boussabatmarwen9@gmail.com>
2025-10-30 09:44:46 +0100

    create signup page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,20 +5,20 @@ const Header = () => {
   return (
     <>
       <div className=" bg-slate-200 shadow-md">
-        <div className="flex  items-center justify-between mx-auto p-3 max-w-6xl">
+        <div className="flex  items-center justify-between mx-auto h-15 max-w-6xl">
           <Link to="/">
             <h1 className="flex flex-wrap text-sm sm:text-xl font-bold ">
               <span className="text-slate-500">Samsar</span>
               <span className="text-slate-700">ProMax</span>
             </h1>
           </Link>
-          <form className="bg-slate-100 p-2 rounded flex items-center">
+          <form className="bg-slate-100  rounded flex items-center">
             <input
               type="text"
               placeholder="search . . ."
               className="
             bg-transparent focus:outline-none 
-            w-24 sm:w-64
+            w-24 sm:w-64 h-8
             "
             />
             <FaSearch className="text-slate-600" />
@@ -26,7 +26,7 @@ const Header = () => {
           <ul className="flex gap-4  font-medium text-slate-600 text-sm  items-center">
             <Link to="/"><li className="hidden sm:inline hover:underline">Home</li></Link>
             <Link to="/about"><li className="hidden sm:inline hover:underline  ">About</li></Link>
-            <Link to="/signin"><li className="hover:underline">Sign In</li></Link>
+            <Link to="/sign-in"><li className="hover:underline">Sign In</li></Link>
           </ul>
         </div>
       </div>
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className ="p-3 max-w-lg mx-auto ">
+      <h1 className='text-2xl text-center font-semibold my-7'>Sign Up Page</h1>
+      <form className="flex flex-col  gap-4 rounded">
+        <input type="text" placeholder='Username'
+        className=' border border-gray-300 p-2 rounded-b-lg mb-2'
+        id='username'
+        />
+        <input type="email" placeholder='Email'
+        className='border border-gray-300 p-2 rounded-lg mb-2'
+        id='email'
+        />
+        <input type="password" placeholder='Password'
+        className='border border-gray-300 p-2 rounded-lg mb-2'
+        id='password'
+        />
+        <button className='bg-slate-700 text-white p-3 uppercase rounded-lg
+          hover:opacity-95 disabled:opacity-80
+        '>SIGN UP</button>
+        <button className='bg-red-700 uppercase text-white p-3 rounded-lg'>continue with Google</button>
+      </form>
+      <div className=' flex gap-2 pt-5'>
+        <p>Already have an account? </p>
+        <Link to="/sign-in" className='text-blue-700 underline'>Sign in</Link>
+      </div>
+    </div>
   )
 }
 
