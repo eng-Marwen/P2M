00bed24fea41442b7cc5491f90b140d4842c1f0c
Marwen Boussabat <boussabatmarwen9@gmail.com>
2026-01-14 11:09:39 +0100

    add index.ts for loading env var

---
+++ backend/src/index.ts
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+
+// Load environment variables FIRST
+dotenv.config();
+
+// Use dynamic import to ensure dotenv runs before server.ts executes
+await import("./server");
