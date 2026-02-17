51981537ad5df623a1e965c8bf4a415b8e1b01f5
Marwen Boussabat <boussabatmarwen9@gmail.com>
2026-02-17 16:57:30 +0100

    token generation testing

--- backend/src/utils/generateTokenAndSetCookie.ts
+++ backend/src/utils/generateTokenAndSetCookie.ts
@@ -1,11 +1,11 @@
-import { Response, CookieOptions } from "express";
+import { CookieOptions, Response } from "express";
 import jwt from "jsonwebtoken";
 
 const isProduction: boolean = process.env.NODE_ENV === "production";
 
 export const generateTokenAndSetCookie = (
   res: Response,
-  userId: string
+  userId: string,
 ): string => {
   const token = jwt.sign({ userId }, process.env.SECRET_KEY as string, {
     expiresIn: "7d",
@@ -14,11 +14,11 @@ export const generateTokenAndSetCookie = (
   console.log("Environment:", process.env.NODE_ENV);
   console.log("Setting cookie for userId:", userId);
 
-  const cookieOptions:CookieOptions = {
+  const cookieOptions: CookieOptions = {
     httpOnly: true,
     secure: isProduction, //false for localhost testing true for production
-    sameSite: (isProduction?"none":"lax"),
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    sameSite: isProduction ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days for production
     path: "/",
   };
 
