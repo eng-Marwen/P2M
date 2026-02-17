aec4f50106f86d8d6025f9d194031a9b19e7ef51
Marwen Boussabat <boussabatmarwen9@gmail.com>
2026-02-17 19:52:40 +0100

    create redis config connection file

---
+++ backend/src/databases/redis.ts
@@ -0,0 +1,14 @@
+import { createClient } from "redis";
+
+export const redisClient = createClient({
+  url: process.env.REDIS_URL, 
+});
+
+redisClient.on("error", (err) => {
+  console.error("Redis error:", err);
+});
+
+export const connectRedis = async () => {
+  await redisClient.connect();
+  console.log("connected to redis");
+};
