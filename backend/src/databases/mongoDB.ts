010144be9cf114d67c53444a2c65d0bfd453d55d
Marwen Boussabat <boussabatmarwen9@gmail.com>
2026-02-17 19:39:14 +0100

    conncetDB -> connectToMongoDB

--- backend/src/databases/mongoDB.ts
+++ backend/src/databases/mongoDB.ts
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () :Promise<void> => { //promise return nothing
+export const connectToMongoDB = async () :Promise<void> => { //promise return nothing
   try {
     await mongoose.connect(process.env.CONNECTION_STRING as string);
-    console.log("Connected to DB");
+    console.log("Connected to mongoDB");
   } catch (error){
     console.log("failed to connect to db , error message: ",(error as Error).message);
     process.exit(1);
