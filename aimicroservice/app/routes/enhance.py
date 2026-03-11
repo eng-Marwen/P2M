67e62e97c7f9208a0ec790bfe161f486d3227ed2
Marwen Boussabat <boussabatmarwen9@gmail.com>
2026-03-11 16:18:17 +0100

    add enhace router

---
+++ aimicroservice/app/routes/enhance.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from app.schemas import DescriptionRequest, DescriptionResponse
+from app.services.enhancer import enhance_description
+
+router = APIRouter()
+
+@router.post("/enhance", response_model=DescriptionResponse)
+async def enhance(data: DescriptionRequest):
+
+    result = await enhance_description(data.description)
+    print(result)
+
+    return {"enhanced_description": result}
