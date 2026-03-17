a9cb476964b99ad99279da69be5e8571cd997e79
Marwen Boussabat <boussabatmarwen9@gmail.com>
2026-03-17 17:45:09 +0100

    connection function

---
+++ backend/src/queue/rabbitmq.ts
@@ -0,0 +1,23 @@
+import amqp, { Channel } from "amqplib";
+
+let channel: Channel;
+
+export const connectRabbitMQ = async () => {
+  try {
+    const connection = await amqp.connect("amqp://localhost");
+
+    channel = await connection.createChannel();
+
+    console.log("RabbitMQ connected");
+  } catch (error) {
+    console.error("RabbitMQ connection error:", error);
+  }
+};
+
+export const getChannel = () => {
+  if (!channel) {
+    throw new Error("RabbitMQ channel not initialized");
+  }
+
+  return channel;
+};
