226e9fe29ce135d9dccc8fcaede41806010b5a37
Marwen Boussabat <boussabatmarwen9@gmail.com>
2026-04-04 17:24:37 +0100

    set up env vars

--- aimicroservice/app/queue/rabbitmq.py
+++ aimicroservice/app/queue/rabbitmq.py
@@ -1,15 +1,24 @@
 import pika
 import os
 
+
 def get_connection():
     rabbitmq_url = os.getenv("RABBITMQ_URL")
 
     if rabbitmq_url:
         params = pika.URLParameters(rabbitmq_url)
     else:
+        rabbitmq_host = os.getenv("RABBITMQ_HOST", "localhost:5672")
+        if ":" in rabbitmq_host:
+            host, port_str = rabbitmq_host.rsplit(":", 1)
+            rabbitmq_port = int(port_str)
+        else:
+            host = rabbitmq_host
+            rabbitmq_port = int(os.getenv("RABBITMQ_PORT", "5672"))
+
         params = pika.ConnectionParameters(
-            host=os.getenv("RABBITMQ_HOST", "localhost"),
-            port=int(os.getenv("RABBITMQ_PORT", "5672")),
+            host=host,
+            port=rabbitmq_port,
             heartbeat=600,
             blocked_connection_timeout=300,
         )
