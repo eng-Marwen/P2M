4d02fbbee404d34d405452055209b3bd8d6a620e
Marwen Boussabat <boussabatmarwen9@gmail.com>
2026-04-08 14:57:27 +0100

    change env var name

--- backend/src/databases/mongoDB.ts
+++ backend/src/databases/mongoDB.ts
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 export const connectMongoDB = async () :Promise<void> => { //promise return nothing
   try {
-    await mongoose.connect(process.env.CONNECTION_STRING as string);
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
     console.log("Connected to mongoDB");
   } catch (error){
-    console.log("failed to connect to db , error message: ",(error as Error).message);
+    console.log("Failed to connect to MongoDB , error message: ",(error as Error).message);
     process.exit(1);
   }
 };
