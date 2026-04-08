c9488b8a0dffddc652333e9ff604beb2ebada425
Marwen Boussabat <boussabatmarwen9@gmail.com>
2026-04-08 15:00:08 +0100

    add missong imports

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="vite/client" />
 
+declare module "*.css";
+declare module "swiper/css";
+declare module "swiper/css/navigation";
+declare module "swiper/css/pagination";
+
 declare module "*.png" {
   const value: string;
   export default value;
