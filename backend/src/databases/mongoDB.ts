876406787aa1b0cd3a52674cd0a8540dfed021ce
Marwen Boussabat <boussabatmarwen9@gmail.com>
2026-04-13 14:55:51 +0100

    change logs  when connected to mongodb

--- backend/src/databases/mongoDB.ts
+++ backend/src/databases/mongoDB.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectMongoDB = async () :Promise<void> => { //promise return nothing
   try {
     await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
-    console.log("Connected to mongoDB");
+    console.log("connected to mongoDB");
   } catch (error){
     console.log("Failed to connect to MongoDB , error message: ",(error as Error).message);
     process.exit(1);
